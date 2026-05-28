cc3b1554e66b0244b057bfb4f2a30870041dbcf8
Ramesh Mahipathi <rameshmahipathi01@gmail.com>
2026-05-28 16:39:24 +0530

    Kubernetes External DNS and Karpenter

---
+++ Kubernetes/12-Terraform_EKS_Cluster_ExternalDNS/01-VPC-terraform-manifests/01-versions.tf
@@ -0,0 +1,27 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
+    }
+  }
+
+  # Remote Backend
+  backend "s3" {
+
+    bucket  = "tfstate-dev-ap-south-1-fez2tn"
+    key     = "vpc/dev/terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+    #use_lockfile = true
+  }
+}
+
+provider "aws" {
+
+  region = var.aws_region
+}
